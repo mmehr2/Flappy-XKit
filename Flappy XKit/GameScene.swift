731e4cd24782e4f3477c8bc0722763edb240e66d
Mike Mehr <mmehr2x-dev@yahoo.com>
2015-09-07 21:03:05 -0700

    Changes for Tutorial Part 2: Player Movement.

--- Flappy XKit/GameScene.swift
+++ Flappy XKit/GameScene.swift
@@ -17,14 +17,22 @@ enum Layer: CGFloat {
 
 class GameScene: SKScene {
     
+    let kGravity: CGFloat = -1500.0 // tweak for best game feel; units are points/s²; 1000 is about earth gravity
+    let kImpulse: CGFloat = 400.0 // tweak for different flap velocity; units are points/s
+    
     let worldNode = SKNode() // makes entire world movable as a unit
     var playableStart = CGFloat(0) // Y position of ground line (where foreground and background images touch)
     var playableHeight = CGFloat(0) // distance from playableStart to top of scene (this is height of background image)
+    let player = SKSpriteNode(imageNamed: "Bird0")
+    var lastUpdateTime: NSTimeInterval = 0
+    var dt: NSTimeInterval = 0
+    var playerVelocity = CGPoint.zeroPoint
 
     override func didMoveToView(view: SKView) {
         addChild(worldNode)
         setupBackground()
         setupForeground()
+        setupPlayer()
     }
     
     // MARK: Setup methods
@@ -47,9 +55,53 @@ class GameScene: SKScene {
         worldNode.addChild(foreground)
     }
     
-    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+    func setupPlayer() {
+        //player.anchorPoint = CGPoint(x: 0.5, y: 0) // middle X, bottom Y; middle X and Y is the default
+        player.position = CGPoint(x: size.width * 0.2, y: playableStart + playableHeight * 0.4)
+        player.zPosition = Layer.Player.rawValue
+        worldNode.addChild(player)
     }
     
+    // MARK: Gameplay
+    
+    func flapPlayer() {
+        // Apply velocity impulse
+        playerVelocity = CGPoint(x: 0, y: kImpulse)
+    }
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        flapPlayer()
+    }
+    
+    // MARK: Updates
+    
     override func update(currentTime: CFTimeInterval) {
+        // called every frame, default 60 fps (or less if complicated)
+        if lastUpdateTime > 0 {
+            dt = currentTime - lastUpdateTime
+        } else {
+            dt = 0
+        }
+        lastUpdateTime = currentTime
+        
+        updatePlayer()
+    }
+    
+    func updatePlayer() {
+        // "mini physics engine" calculations
+        // Apply gravity
+        let gravity = CGPoint(x: 0, y: kGravity) // might not be straight up/down
+        let velocityStepDueToGravity = gravity * CGFloat(dt)
+        playerVelocity += velocityStepDueToGravity
+        
+        // Apply velocity
+        let positionStepDueToVelocity = playerVelocity * CGFloat(dt)
+        player.position += positionStepDueToVelocity
+        
+        // Ground check halt (temporary solution)
+        let playerBottomDistanceFromMiddle = player.size.height/2
+        if player.position.y - playerBottomDistanceFromMiddle < playableStart {
+            player.position.y = playableStart + playerBottomDistanceFromMiddle
+        }
     }
 }
